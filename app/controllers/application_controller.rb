83ebf569590e6e98643fcb5dd695f31c263f7702
nori1jz <apriotype2xxnorixx@gmail.com>
2021-08-21 19:39:25 +0000

    [add]フラッシュメッセージ

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,7 @@ class ApplicationController < ActionController::Base
   # def before_sign_in_path_for
   #   root_path
   # end
+  protected
 
   def after_sign_in_path_for(resource)
     user_path(resource)
@@ -13,10 +14,16 @@ class ApplicationController < ActionController::Base
   def after_sign_out_path_for(resource)
     root_path
   end
-
-  protected
-
+  
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+      if resource_class == User
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email])
+        devise_parameter_sanitizer.permit(:sign_in,keys:[:email])
+        devise_parameter_sanitizer.permit(:account_update,keys:[:name,:email])
+      end
+    end
   end
-end
+  # def configure_permitted_parameters
+  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  # end
+
